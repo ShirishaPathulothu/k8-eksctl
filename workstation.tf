269eafc750552b5304956b23133311266282195b
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-04 16:11:18 +0530

    instance

--- workstation.tf
+++ workstation.tf
@@ -3,5 +3,6 @@ module "workstation" {
     ami_id = var.ami_id
     instance_type = var.instance_type
     public_subnet_id = var.public_subnet_id
+    user_data = file("${path.module}/workstation.sh")
     tags = var.tags
 }
