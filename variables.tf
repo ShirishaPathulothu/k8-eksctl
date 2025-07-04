5c0b84408287629ab1d4b42fa964caa0a2e88df2
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-04 13:41:06 +0530

    instance

--- variables.tf
+++ variables.tf
@@ -3,9 +3,25 @@ variable "public_subnet_id" {
     default = "subnet-04b690ffdaa49022a"
 }
 
-variable "sg_id" {
-    type = list(string)
-    default = ["sg-0f81b4954c6b01728"]
+
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
 }
 
+# variable "user_data" {
+#     type = string
+# }
+
+variable "tags" {
+    default = {
+        Name = "workstation"
+    }
+}
+
+
+variable "ami_id" {
+    default = "ami-09c813fb71547fc4f"
+}
 
